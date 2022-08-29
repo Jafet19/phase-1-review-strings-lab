19d9c4cee580eafbb5a1b96d36dead1a49a4e363
Jafet19 <kacur99@gmail.com>
2022-08-29 10:18:03 -0500

    commit tree 4a1d7ca1bbda9a396d48f30a1b67ad460d6123e8

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Jafet';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + "!";
+let shortGreeting = "Welcome, " + currentUser[0]+ "!";
